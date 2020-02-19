ef376acc4a3be3c8d20fc931721de57c21432b45
Alberto Rosales <arosales7@ucol.mx>
2020-02-19 02:49:41 -0600

    Avance

--- doctor.js
+++ doctor.js
@@ -1,25 +1,32 @@
+import Nombre from "./nombre.js";
+
 class Doctor {
-    /**
-     * 
-     * @param {string} nombre El nombre del doctor
-     * @param {string} especialidad Especialidad del doctor
-     * @param {string} telefono Telefono del doctor
-     * @param {string} cedula Cedula profesional del doctor
-     */
+  /**
+   *
+   * @param {Nombre} nombre El nombre del doctor
+   * @param {string} especialidad Especialidad del doctor
+   * @param {string} telefono Telefono del doctor
+   * @param {string} cedula Cedula profesional del doctor
+   */
 
-    constructor (nombre, especialidad, telefono, cedula) {
-        this.nombre = nombre;
-        this.especialidad = especialidad;
-        this.telefono = telefono;
-        this.cedula = cedula;
-    }
+  constructor(nombre, especialidad, telefono, cedula) {
+    this.nombre = nombre;
+    this.especialidad = especialidad;
+    this.telefono = telefono;
+    this.cedula = cedula;
+  }
 
-    getPerfil() {
-
-        return `${this.cedula} ${this.especialidad} ${this.nombre} ${this.telefono}`
-    }
+  getPerfil() {
+    return `${this.cedula} ${this.especialidad} ${this.nombre.getNombreCompleto()} ${this.telefono}`;
+  }
 }
 /*
-let doctor1 = new Doctor("J. Felix Rosales González", "Médico cirugano del aparato digestivo", "312-106-0505", "12345678");
+let doctor1 = new Doctor(
+  new Nombre("Felix", "Rosales", "González"),
+  "Médico Cirugano",
+  "312-106-0505",
+  "12345678"
+);
+
 console.log(doctor1.getPerfil());
 */
