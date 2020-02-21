1721e4eed7d78d9d95c5b5737ed22c4eaa2eae6e
Alberto Rosales <arosales7@ucol.mx>
2020-02-20 23:30:08 -0600

    Cita terminado

--- cita.js
+++ cita.js
@@ -4,13 +4,13 @@ import Fecha from "./fecha.js"
 import Tiempo from "./tiempo.js"
 import Nombre from "./nombre.js"
 
-class Cita {
+export default class Cita {
     /**
      * 
      * @param {Fecha} fecha la fecha de la cita
      * @param {Tiempo} hora la hora de la cita
-     * @param {Nombre} doctor el doctor de la cita
-     * @param {Nombre} paciente el paciente de la cita
+     * @param {Doctor} doctor el doctor de la cita
+     * @param {Paciente} paciente el paciente de la cita
      */
     constructor(fecha, hora, doctor, paciente) {
         this.fecha = fecha;
@@ -20,14 +20,9 @@ class Cita {
     }
 
     getCita() {
-        this.fecha = new Fecha(15, 3, 2020)
-        this.hora = new Tiempo(5,30, "pm")
-        this.doctor = new Nombre("Dr. Felix", "Rosales", "González")
-        this.paciente = new Nombre("Alberto", "Rosales", "González")
-        return `${this.fecha.getFecha()}, ${this.hora.getFormato24()}, ${this.doctor.getNombreCompleto()}, ${this.paciente.getNombreCompleto()}`
-    }
+        return `${this.fecha.getFecha()}, ${this.hora.getFormato24()}, ${"Dr. " +
+          this.doctor.doctor
+            .apellidoPaterno}, ${this.paciente.nombre.getNombreCompleto()}`;
+      }
 
 }
-
-let app = new Cita
-console.log(app.getCita())
--- fecha.js
+++ fecha.js
@@ -1,130 +1,73 @@
 export default class Fecha {
   /**
    *
-   * @param {number} dia valor 1...31
-   * @param {number} mes valor 1...12
-   * @param {number} año año de la fecha de nacimiento
+   * @param {number} dia |Valor 1...32
+   * @param {number} mes |Valor 1...12
+   * @param {number} año |Año de la fecha
    */
+
   constructor(dia, mes, año) {
     this.fecha = new Date(año, mes - 1, dia);
     this.diaSemana = [
       "Domingo",
       "Lunes",
       "Martes",
-      "Miercoles",
+      "Miércoles",
       "Jueves",
       "Viernes",
-      "Sabado"
+      "Sábado"
     ];
-    this.diaMes = [
-      "Enero",
-      "Febrero",
-      "Marzo",
-      "Abril",
-      "Mayo",
-      "Junio",
-      "Julio",
-      "Agosto",
-      "Septiembre",
-      "Octubre",
-      "Noviembre",
-      "Diciembre"
+    this.mes = [
+      "Ene",
+      "Feb",
+      "Mar",
+      "Abr",
+      "May",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dic"
     ];
-    this.fecha2 = new Date();
-  }
-
-  getEdad() {
-    let edad = this.fecha2.getUTCFullYear() - this.fecha.getFullYear();
-    return `Edad de ${edad} años`;
-  }
-  getFormatoCorto() {
-    let date = this.fecha.getDate();
-    let month = this.diaMes[this.fecha.getMonth()];
-    let month1 = this.fecha.getMonth() + 1;
-    let year = this.fecha.getFullYear();
-    return `${year}-${month1}-${date}`;
-  }
-
-  getFormatoExtendido() {
-    let dia = this.diaSemana[this.fecha.getDay()];
-    let numero = this.fecha.getDate();
-    let mes = this.diaMes[this.fecha.getMonth()];
-    let año = this.fecha.getFullYear();
-    return dia, numero, "de", mes, "de", año;
   }
 
   getAños() {
-    let año = this.fecha.getFullYear() - this.fecha2.getUTCFullYear();
-    return año;
+    let x = Date.now() - this.fecha;
+    let z = 1000 * 60 * 60 * 24 * 365;
+    let fechaActual = Math.trunc(x / z);
+    return `La diferencia es de ${fechaActual} años`;
   }
 
   getMeses() {
-    let año = this.fecha.getFullYear() - this.fecha2.getUTCFullYear();
-    if (año > 0) {
-      var mes = año * 12 + this.fecha.getMonth() - this.fecha2.getUTCMonth();
-    } else {
-      var mes = this.fecha.getMonth() - this.fecha2.getUTCMonth();
-    }
-    return mes;
+    // PREGUNTAR AL PROFE
+    let x = Date.now() - this.fecha;
+    let z = 1000 * 60 * 60 * 24 * 7 * 4;
+    let fechaActual = Math.trunc(x / z);
+    return `La diferencia es de ${fechaActual} meses`;
   }
 
   getSemanas() {
-    let año = this.fecha.getFullYear() - this.fecha2.getUTCFullYear();
-    if (año > 0) {
-      var mes = año * 12 + this.fecha.getMonth() - this.fecha2.getUTCMonth();
-      var semanas = mes * 4
-    } else if (año < 1) {
-      var mes = this.fecha.getMonth() - this.fecha2.getUTCMonth();
-      var semanas = mes * 4
-      
-    }if (semanas <= 0) {
-        var semanas = (this.fecha.getDate() - this.fecha2.getUTCDate()) / 7
-      }
-    return Math.trunc(semanas);
+    let x = Date.now() - this.fecha;
+    let z = 1000 * 60 * 60 * 24 * 7;
+    let fechaActual = Math.trunc(x / z);
+    return `La diferencia es de ${fechaActual} semanas`;
   }
 
   getDias() {
-    let año = this.fecha.getFullYear() - this.fecha2.getUTCFullYear();
-    if (año > 0) {
-      var mes = año * 12 + this.fecha.getMonth() - this.fecha2.getUTCMonth();
-      var semanas = mes * 4
-    } else if (año < 1) {
-      var mes = this.fecha.getMonth() - this.fecha2.getUTCMonth();
-      var semanas = mes * 4
-      
-    }if (semanas <= 0) {
-        var semanas = (this.fecha.getDate() - this.fecha2.getUTCDate()) / 7
-      }
-
-    if (mes > 0) {
-      let x = this.fecha - Date.now(); 
-      let z = 1000 * 60 * 60 * 24;
-      var dias = Math.trunc(x / z);
-
-    } else {
-      var dias = (this.fecha.getDate() - this.fecha2.getUTCDate())
-    }
-    return dias;
+    let x = Date.now() - this.fecha;
+    let z = 1000 * 60 * 60 * 24;
+    let fechaActual = Math.trunc(x / z);
+    return `La diferencia es de ${fechaActual} días`;
   }
 
   getFecha() {
-    let date = this.fecha.getDate();
-    let month = this.diaMes[this.fecha.getMonth()];
-    let year = this.fecha.getFullYear();
-    return `${date}/${month}/${year}`;
+    return `${this.fecha.getDate()}/${
+      this.mes[this.fecha.getMonth()]
+    }/${this.fecha.getFullYear()}`;
   }
-
   getDiaFecha() {
-    let dia = this.diaSemana[this.fecha.getDay()];
-    return dia;
+    return `${this.diaSemana[this.fecha.getDay()]}`;
   }
 }
-/*
-let fecha1 = new Fecha(29, 2, 2030);
-console.log(fecha1.getAños());
-console.log(fecha1.getMeses());
-console.log(fecha1.getSemanas());
-console.log(fecha1.getDias());
-console.log(fecha1.getFecha());
-console.log(fecha1.getDiaFecha());
-*/
